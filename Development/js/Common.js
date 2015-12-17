bde5ae63bae84120921d868124fd347219c6616d
dstiefe <dstiefe@gmail.com>
2015-12-16 21:46:57 -0500

    created development folder

---
+++ Development/js/Common.js
@@ -0,0 +1,16 @@
+
+var ValiDatedTokenObject =
+{
+    access_token: "",
+    token_type: "",
+    expires_in: "",
+    userName: "",
+    issued: "",
+    expires: ""
+}
+
+
+$(document).ready(function () {
+
+    $('.splash').hide();
+})

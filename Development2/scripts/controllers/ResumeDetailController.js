52abab2888e5984e3250f95beaf308e1c582cf77
kornilov <kornilov@tradeinchat.com>
2016-01-18 17:46:08 +0500

    fix

--- Development2/scripts/controllers/ResumeDetailController.js
+++ Development2/scripts/controllers/ResumeDetailController.js
@@ -1,13 +1,14 @@
 /**
  * Created by Van on 17.01.2016.
  */
-angular.module('Jobsite').controller('ResumeDetailController', function ($scope, $modalInstance, resume, text,ResumesService, $sce, $timeout,$document) {
+angular.module('Jobsite').controller('ResumeDetailController', function ($scope, $modalInstance, resume, text, ResumesService, $sce, $timeout,$document) {
 
     $scope.searchText = (!angular.isUndefinedOrNull(text))? text :'';
     $scope.id = resume.parentDocumentId;
     $scope.resumeSource = '';
     $scope.currentPage = resume.pageNumber;
     $scope.totalPages = resume.totalPages;
+    $scope.resume = resume;
     $scope.itemsPerPage = 1;
     $scope.maxSize = 5;
 
@@ -18,41 +19,8 @@ angular.module('Jobsite').controller('ResumeDetailController', function ($scope,
             if (location.pathname.indexOf('Development2') != -1){
                 host = location.protocol + '//' + location.host + '/Development2';
             }
-
             var viewrUrl = host + "/pdf-viewer/web/viewer.html?file=" + window.escape(_url) + "#page=1&zoom=page-fit&search=" + $scope.searchText + "&phrase=false&hlall=true";
-           // $scope.resumeSource = 'about:blank';
-           // $('#pdfViewer').val('');
-           // $('#pdfViewer').empty();
-           // $document.find("#pdfViewer").empty();
-           // angular.element(document.querySelector('#pdfViewer')).empty();
-
-            //$scope.resumeSource = viewrUrl;
-
-
-           // $document.find("#pdfViewer").remove();
             $scope.resumeSource = viewrUrl;
-            //var r = $document.find("#pdfViewer");
-            //r[0].contentWindow.location.reload(true);
-            //
-            //$timeout( function(){
-            //    console.log('empty frame');
-            //    debugger;
-            //
-            //    $scope.resumeSource = viewrUrl;
-            //    //var r = $document.find("#pdfViewer");
-            //    //var r = $document.find("#pdfViewer");
-            //    ////r[0].contentWindow.location.reload(true);
-            //    //r[0].contentDocument.location.reload(true);
-            //    //r.attr("src",r.attr("src"));
-            //    //$document.find("#pdfViewer")[0].empty();
-            //
-            //   // angular.element(document.querySelector('#pdfViewer')).empty();
-            //}, 3000);
-            //var myEl = angular.element( document.querySelector( '#pdfViewer' ) );
-            //debugger;
-            //myEl.context.contentWindow.location.reload(true);
-
-
         }, function (error) {
             console.log(error.data.message);
             $scope.resumeSource = '';
@@ -62,9 +30,11 @@ angular.module('Jobsite').controller('ResumeDetailController', function ($scope,
 
 
     $scope.search = function () {
-        $scope.currentPage = 1;
+
         ResumesService.searchIntoResume($scope.id, $scope.searchText).then(function (results) {
             $scope.resume = results.data;
+            $scope.currentPage = resume.pageNumber;
+            $scope.totalPages = resume.totalPages;
             _getResumeSource();
         }, function (error) {
             console.log(error.data.message);
@@ -72,13 +42,16 @@ angular.module('Jobsite').controller('ResumeDetailController', function ($scope,
 
     };
 
+
     $scope.pageChanged = function() {
-
-
         _getResumeSource();
     };
 
 
+    $scope.ClickOnNeighborsPage = function(page, total) {
+        $scope.currentPage = page;
+        _getResumeSource();
+    };
 
     $scope.close = function () {
         $modalInstance.close();

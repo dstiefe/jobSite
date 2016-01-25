8568fee9063b7465683846a24065da5c6a9a9f74
kornilov <kornilov@tradeinchat.com>
2016-01-25 16:14:34 +0500

    filer employe type

--- Development2/scripts/controllers/searchjobsController.js
+++ Development2/scripts/controllers/searchjobsController.js
@@ -1,5 +1,5 @@
 angular
-    .module('Jobsite').controller("searchjobController", function($scope, Login,ValiDatedTokenObject, $location,$http, $timeout) {
+    .module('Jobsite').controller("searchjobController", function($scope, Login,ValiDatedTokenObject, $location,$http, $timeout, RESOURCES) {
 
         if (sessionStorage.getItem("ValiDatedTokenObject") == null) { } else {
             ValiDatedTokenObject.setValiDatedTokenObject(JSON.parse(sessionStorage.getItem("ValiDatedTokenObject")));
@@ -9,21 +9,45 @@ angular
         $scope.searchtext = "";
         $scope.categoryId = '';
         $scope.locationId = '';
+        $scope.employeeType = '';
+        $scope.EmployeeTypes = RESOURCES.EMPLOYEE_TYPES;
         //$scope.currentPage = 1;
         //$scope.maxSize = 10;
         //$scope.itemsPerPage = 10;
         //$scope.totalItems = 0;
         //$scope.maxSize = 5;
 
-        var req = {
+        var _searchByFilter = function() {
+
+        var params = {};
+        params['count'] = 1000;
+        if ($scope.locationId != '') {
+            params['locationId'] = $scope.locationId;
+        }
+
+        if ($scope.categoryId != '') {
+            params['categoryId'] = $scope.categoryId;
+        }
+
+        if ($scope.searchtext != ''){
+            params['text'] = $scope.searchtext;
+        }
+
+        if ($scope.employeeType != '') {
+            params['employeeType'] = $scope.employeeType;
+        }
+
+        var query = jQuery.param(params);
+        var serviceUrl = ServicesURL + 'api/v1/jobs/search?'+query;
+
+        var req1 = {
             method: 'GET',
-            url: ServicesURL + 'api/v1/jobs/search?count=1000',
+            url: serviceUrl,
             headers: {
                 'Content-Type': 'application/json'
             }
-        };
-
-        $http(req).then(function(data) { 
+        }
+        $http(req1).then(function(data) {
             if (data.status == "200") {
                 $scope.list = data.data;
                 var locations = [];
@@ -66,72 +90,19 @@ angular
                 $scope.categories = categories;
                 $scope.currentPage = 1; //current page
                 $scope.entryLimit = 10; //max no of items to display in a page
-                $scope.filteredItems = $scope.list.length; //Initially for no filter  
+                $scope.filteredItems = $scope.list.length; //Initially for no filter
                 $scope.totalItems = $scope.list.length;
             }
         });
+    }
+
         $scope.searchjob = function() {
-            var searchtext = "?count=1000";
+
             $scope.categoryId = '';
             $scope.locationId = '';
+            $scope.employeeType = '';
 
-            if ($scope.searchtext != "") {
-                searchtext += '&text=' + $scope.searchtext;
-            }
-
-            var req1 = {
-                method: 'GET',
-                url: ServicesURL + 'api/v1/jobs/search' + searchtext + '',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-            $http(req1).then(function(data) {
-                if (data.status == "200") {
-                    $scope.list = data.data;
-                    var locations = [];
-                    var categories = [];
-                    for (var k = 0; k < data.data.length; k++) {
-                        var visibleornot = true;
-                        for (l = 0; l < locations.length; l++) {
-                            if (locations[l].location == data.data[k].location) {
-                                visibleornot = false;
-                                locations[l].count = locations[l].count + 1;
-                            }
-                        }
-                        if (visibleornot) {
-                            locations.push({
-                                location: data.data[k].location,
-                                id: data.data[k].locationId,
-                                count: 1
-                            });
-                        }
-                    }
-                    for (var k = 0; k < data.data.length; k++) {
-
-                        var visibleornot = true;
-                        for (l = 0; l < categories.length; l++) {
-                            if (categories[l].category == data.data[k].category) {
-                                visibleornot = false;
-                                categories[l].count = categories[l].count + 1;
-                            }
-                        }
-                        if (visibleornot) {
-                            categories.push({
-                                category: data.data[k].category,
-                                id: data.data[k].categoryId,
-                                count: 1
-                            });
-                        }
-                    }
-                    $scope.locations = locations;
-                    $scope.categories = categories;
-                    $scope.currentPage = 1; //current page
-                    $scope.entryLimit = 10; //max no of items to display in a page
-                    $scope.filteredItems = $scope.list.length; //Initially for no filter  
-                    $scope.totalItems = $scope.list.length;
-                }
-            });
+            _searchByFilter();
         };
         $scope.searchbycategory = function(categoryid) {
 
@@ -140,78 +111,7 @@ angular
             }else{
                 $scope.categoryId = '';
             }
-
-
-            var params = {};
-            params['count'] = 1000;
-            if ($scope.locationId != '') {
-                params['locationId'] = $scope.locationId;
-            }
-
-            if ($scope.categoryId != '') {
-                params['categoryId'] = $scope.categoryId;
-            }
-
-            if ($scope.searchtext != ''){
-                params['text'] = $scope.searchtext;
-            }
-
-            var query = jQuery.param(params);
-            var serviceUrl = ServicesURL + 'api/v1/jobs/search?'+query;
-
-            var req1 = {
-                method: 'GET',
-                url: serviceUrl,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-            $http(req1).then(function(data) {
-                if (data.status == "200") {
-                    $scope.list = data.data;
-                    var locations = [];
-                    var categories = [];
-                    for (var k = 0; k < data.data.length; k++) {
-                        var visibleornot = true;
-                        for (l = 0; l < locations.length; l++) {
-                            if (locations[l].location == data.data[k].location) {
-                                visibleornot = false;
-                                locations[l].count = locations[l].count + 1;
-                            }
-                        }
-                        if (visibleornot) {
-                            locations.push({
-                                location: data.data[k].location,
-                                id: data.data[k].locationId,
-                                count: 1
-                            });
-                        }
-                    }
-                    for (var k = 0; k < data.data.length; k++) {
-
-                        var visibleornot = true;
-                        for (l = 0; l < categories.length; l++) {
-                            if (categories[l].category == data.data[k].category) {
-                                visibleornot = false;
-                                categories[l].count = categories[l].count + 1;
-                            }
-                        }
-                        if (visibleornot) {
-                            categories.push({
-                                category: data.data[k].category,
-                                id: data.data[k].categoryId,
-                                count: 1
-                            });
-                        }
-                    }
-                    $scope.locations = locations;
-                    $scope.categories = categories;
-                    $scope.currentPage = 1; //current page
-                    $scope.entryLimit = 10; //max no of items to display in a page
-                    $scope.filteredItems = $scope.list.length; //Initially for no filter  
-                    $scope.totalItems = $scope.list.length;
-                }
-            });
+            _searchByFilter();
         };
         $scope.searchbylocation = function(locationId) {
 
@@ -220,78 +120,12 @@ angular
             }else{
                 $scope.locationId = '';
             }
-            var params = {};
-            params['count'] = 1000;
-            if ($scope.locationId != '') {
-                params['locationId'] = $scope.locationId;
-               }
-
-            if ($scope.categoryId != '') {
-                params['categoryId'] = $scope.categoryId;
-            }
-
-            if ($scope.searchtext != ''){
-                params['text'] = $scope.searchtext;
-            }
-
-            var query = jQuery.param(params);
-            var serviceUrl = ServicesURL + 'api/v1/jobs/search?'+query;
-
-            var req1 = {
-                method: 'GET',
-                url: serviceUrl,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-            $http(req1).then(function(data) {
-                if (data.status == "200") {
-                    $scope.list = data.data;
-                    var locations = [];
-                    var categories = [];
-                    for (var k = 0; k < data.data.length; k++) {
-
-                        var visibleornot = true;
-                        for (l = 0; l < locations.length; l++) {
-                            if (locations[l].location == data.data[k].location) {
-                                visibleornot = false;
-                                locations[l].count = locations[l].count + 1;
-                            }
-                        }
-                        if (visibleornot) {
-                            locations.push({
-                                location: data.data[k].location,
-                                id: data.data[k].locationId,
-                                count: 1
-                            });
-                        }
-                    }
-                    for (var k = 0; k < data.data.length; k++) {
-
-                        var visibleornot = true;
-                        for (l = 0; l < categories.length; l++) {
-                            if (categories[l].category == data.data[k].category) {
-                                visibleornot = false;
-                                categories[l].count = categories[l].count + 1;
-                            }
-                        }
-                        if (visibleornot) {
-                            categories.push({
-                                category: data.data[k].category,
-                                id: data.data[k].categoryId,
-                                count: 1
-                            });
-                        }
-                    }
-                    $scope.locations = locations;
-                    $scope.categories = categories;
-                    $scope.currentPage = 1; //current page
-                    $scope.entryLimit = 10; //max no of items to display in a page
-                    $scope.filteredItems = $scope.list.length; //Initially for no filter  
-                    $scope.totalItems = $scope.list.length;
-                }
-            });
+            _searchByFilter();
         };
+        $scope.searchByFilter= function() {
+            _searchByFilter();
+        };
+
         $scope.setPage = function(pageNo) {
             $scope.currentPage = pageNo;
         };
@@ -305,4 +139,5 @@ angular
             $scope.reverse = !$scope.reverse;
         };
 
+        _searchByFilter();
     });

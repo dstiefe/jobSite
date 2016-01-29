d036a7b765a30b5d98f8560bc9617297d51a29cc
kornilov <kornilov@tradeinchat.com>
2016-01-29 17:07:20 +0500

    recovery password

--- Development2/scripts/controllers/ResetPasswordVerifyController.js
+++ Development2/scripts/controllers/ResetPasswordVerifyController.js
@@ -8,6 +8,9 @@ angular.module('Jobsite').controller("ResetPasswordVerifyController", function($
 
     $scope.password='';
     $scope.confirmPassword='';
+    $scope.error_Description ='';
+    $scope.success_Description = '';
+
     var startTimer = function () {
         var timer = $timeout(function () {
             $timeout.cancel(timer);
@@ -15,19 +18,26 @@ angular.module('Jobsite').controller("ResetPasswordVerifyController", function($
             $location.path('/login');
         }, 2000);
     };
-    $scope.ChangePassword = function(){
 
+    $scope.ChangePassword = function(isValid){
+        $scope.error_Description ='';
+        $scope.success_Description = '';
+        if (!isValid){
+            return;
+        }
         if ($scope.password != $scope.confirmPassword){
             $scope.error_Description = "Passwords does not match!";
             return;
         }
+
+
         var data = {
             newPassword: $scope.password,
             userId: searchObject.userid,
             code: searchObject.code
         };
         AuthService.resetPassword(data).then(function (results) {
-            $scope.success_Description= 'Password successfully changed, you will be redicted to login page in 2 seconds.';
+            $scope.success_Description = 'Password successfully changed, you will be redicted to login page in 2 seconds.';
             startTimer();
         }, function (response) {
 

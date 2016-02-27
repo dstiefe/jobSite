f4b2c03f67a469035509ef92282fa73286995b2f
Van Korn <vanderkorn@gmail.com>
2016-02-28 01:09:15 +0500

    slide 22

--- Development2/scripts/controllers/ReferralsController.js
+++ Development2/scripts/controllers/ReferralsController.js
@@ -2,26 +2,26 @@
  * Created by Van on 23.02.2016.
  */
 
-angular.module('Jobsite').controller('ReferralsController', function ($scope, $modalInstance, JobsService, ScreeningsService, $sce, $timeout, $document, resume, jobId) {
+angular.module('Jobsite').controller('ReferralsController', function ($scope, JobsService, ScreeningsService, $sce, $timeout, $document) {
 
-    $scope.resume = resume;
-
-    JobsService.getJob(jobId).then(function (results) {
-        response = results.data;
-        $scope.job = response;
-    }, function (error) {
-        console.log(error.data.message);
-    });
-
-
-    ScreeningsService.getTestResultsByResumeId($scope.resume.id).then(function (results) {
-        response = results.data;
-        $scope.results = response;
-    }, function (error) {
-        console.log(error.data.message);
-    });
-    $scope.onClose = function() {
-        $modalInstance.close();
-    }
+    //$scope.resume = resume;
+    //
+    //JobsService.getJob(jobId).then(function (results) {
+    //    response = results.data;
+    //    $scope.job = response;
+    //}, function (error) {
+    //    console.log(error.data.message);
+    //});
+    //
+    //
+    //ScreeningsService.getTestResultsByResumeId($scope.resume.id).then(function (results) {
+    //    response = results.data;
+    //    $scope.results = response;
+    //}, function (error) {
+    //    console.log(error.data.message);
+    //});
+    //$scope.onClose = function() {
+    //    $modalInstance.close();
+    //}
 
 });

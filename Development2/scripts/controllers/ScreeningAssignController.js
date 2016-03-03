c3b079f1ace3b5d279fe7f684a99b2c12d96ea79
kornilov <kornilov@tradeinchat.com>
2016-03-03 17:06:55 +0500

    slide 8

--- Development2/scripts/controllers/ScreeningAssignController.js
+++ Development2/scripts/controllers/ScreeningAssignController.js
@@ -4,35 +4,74 @@
 /**
  * Created by Van on 03.02.2016.
  */
-angular.module('Jobsite').controller("ScreeningAssignController", function($scope, Login, $http, $timeout, $location, ScreeningsService, CategoriesService, JobsService, $state, $stateParams) {
+angular.module('Jobsite').controller("ScreeningAssignController", function($scope, Login, $http, $timeout, $location,$filter, ScreeningsService, JobsService, $state, $stateParams) {
 
-    $scope.id = $stateParams.id;
-    $scope.currentPage = 1;
-    $scope.entryLimit = 10;
-    ScreeningsService.getScreening($scope.id).then(function (results) {
-        $scope.screening = results.data
+    $scope.myScreeningId = $stateParams.id;
+    $scope.selectedJobId = '';
+    // get jobs
+    JobsService.getMyJobs().then(function (results) {
+        $scope.jobs = results.data
     }, function (error) {
         console.log(error.data.message);
     });
 
-    ScreeningsService.getScreeningQuestionsByScreeningId($scope.id).then(function (results) {
-        $scope.screeningQuestions = results.data;
-
-        $scope.currentPage = 1; //current page
-        $scope.entryLimit = 10; //max no of items to display in a page
-        $scope.filteredItems = $scope.screeningQuestions.length; //Initially for no filter
-        $scope.totalItems = $scope.screeningQuestions.length;
-
-    }, function (error) {
-        console.log(error.data.message);
-    });
-
-    $scope.setPage = function(pageNo) {
-        $scope.currentPage = pageNo;
+    var _getScreeningById =function(screeningId) {
+        return $filter('filter')($scope.screenings, {id:screeningId})[0];
     };
-    $scope.sort_by = function(predicate) {
-        $scope.predicate = predicate;
-        $scope.reverse = !$scope.reverse;
+
+    var _getScreeningsByJobId = function() {
+        var items = $filter('filter')($scope.screenings, $scope.jobExist);
+        return $filter('orderBy')(items, 'sort');
+    };
+
+    ////get screening
+    //ScreeningsService.getScreening($scope.myScreeningId).then(function (results) {
+    //    $scope.myScreening = results.data
+    //}, function (error) {
+    //    console.log(error.data.message);
+    //});
+
+    //get my screenings
+    ScreeningsService.getMyScreenings().then(function (results) {
+        $scope.screenings = results.data;
+        $scope.myScreening = _getScreeningById($scope.myScreeningId);
+    }, function (error) {
+        console.log(error.data.message);
+    });
+
+    $scope.saveChanges = function() {
+        ScreeningsService.putScreening($scope.myScreeningId, $scope.myScreening).then(function (results) {
+            $state.go('editscreening', {'id': $scope.myScreeningId});
+        }, function (error) {
+            console.log(error.data.message);
+        });
+    };
+
+
+    $scope.jobExist = function(item) {
+        if (item.jobsIds.indexOf($scope.selectedJobId) != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+
+    $scope.addMyScreening = function() {
+        if ($scope.myScreening.jobsIds.indexOf($scope.selectedJobId) == -1) {
+            var sort = $scope.myScreening.sort;
+
+            var _screenings = _getScreeningsByJobId();
+
+            if (_screenings !=null && _screenings.length > 0){
+               sort = _screenings[_screenings.length-1].sort;
+               sort++;
+            }
+
+            $scope.myScreening.sort = sort;
+            $scope.myScreening.jobsIds.push($scope.selectedJobId);
+        }
     };
 
 });

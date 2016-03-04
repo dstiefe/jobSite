3ed295763023650f18656de1d9aaa0ddf374d8c2
kornilov <kornilov@tradeinchat.com>
2016-03-04 17:56:02 +0500

    referrals

--- Development2/scripts/controllers/ReferralAssignController.js
+++ Development2/scripts/controllers/ReferralAssignController.js
@@ -7,9 +7,9 @@
 /**
  * Created by Van on 03.02.2016.
  */
-angular.module('Jobsite').controller("ReferralAssignController", function($scope, Login, $http, $timeout, $location,$filter, ScreeningsService, JobsService, $state, $stateParams) {
+angular.module('Jobsite').controller("ReferralAssignController", function($scope, Login, $http, $timeout, $location,$filter, ReferralService, JobsService, $state, $stateParams) {
 
-    $scope.myScreeningId = $stateParams.id;
+    $scope.myReferralId = $stateParams.id;
     $scope.selectedJobId = '';
     // get jobs
     JobsService.getMyJobs().then(function (results) {
@@ -18,33 +18,27 @@ angular.module('Jobsite').controller("ReferralAssignController", function($scope
         console.log(error.data.message);
     });
 
-    var _getScreeningById =function(screeningId) {
-        return $filter('filter')($scope.screenings, {id:screeningId})[0];
+    var _getReferralById =function(referralId) {
+        return $filter('filter')($scope.referrals, {id:referralId})[0];
     };
 
-    var _getScreeningsByJobId = function() {
-        var items = $filter('filter')($scope.screenings, $scope.jobExist);
+    var _getReferralsByJobId = function() {
+        var items = $filter('filter')($scope.referrals, $scope.jobExist);
         return $filter('orderBy')(items, 'sort');
     };
 
-    ////get screening
-    //ScreeningsService.getScreening($scope.myScreeningId).then(function (results) {
-    //    $scope.myScreening = results.data
-    //}, function (error) {
-    //    console.log(error.data.message);
-    //});
 
     //get my screenings
-    ScreeningsService.getMyScreenings().then(function (results) {
-        $scope.screenings = results.data;
-        $scope.myScreening = _getScreeningById($scope.myScreeningId);
+    ReferralService.getMyJobReferrals().then(function (results) {
+        $scope.referrals = results.data;
+        $scope.myReferral = _getReferralById($scope.myReferralId);
     }, function (error) {
         console.log(error.data.message);
     });
 
     $scope.saveChanges = function() {
-        ScreeningsService.putScreening($scope.myScreeningId, $scope.myScreening).then(function (results) {
-            $state.go('editscreening', {'id': $scope.myScreeningId});
+        $scope.putJobReferral($scope.myReferralId, $scope.myReferral).then(function (results) {
+            $state.go('editreferral', {'id': $scope.myReferralId});
         }, function (error) {
             console.log(error.data.message);
         });
@@ -61,19 +55,19 @@ angular.module('Jobsite').controller("ReferralAssignController", function($scope
 
 
 
-    $scope.addMyScreening = function() {
-        if ($scope.myScreening.jobsIds.indexOf($scope.selectedJobId) == -1) {
-            var sort = $scope.myScreening.sort;
+    $scope.addMyReferral = function() {
+        if ($scope.myReferral.jobsIds.indexOf($scope.selectedJobId) == -1) {
+            var sort = $scope.myReferral.sort;
 
-            var _screenings = _getScreeningsByJobId();
+            var _screenings = _getReferralsByJobId();
 
             if (_screenings !=null && _screenings.length > 0){
                 sort = _screenings[_screenings.length-1].sort;
                 sort++;
             }
 
-            $scope.myScreening.sort = sort;
-            $scope.myScreening.jobsIds.push($scope.selectedJobId);
+            $scope.myReferral.sort = sort;
+            $scope.myReferral.jobsIds.push($scope.selectedJobId);
         }
     };
 

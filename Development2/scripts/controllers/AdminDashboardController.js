bab965e574e6d0ac0a995feffeaac1293ff00742
Van Korn <vanderkorn@gmail.com>
2016-04-02 02:31:14 +0500

    updates25 starts

---
+++ Development2/scripts/controllers/AdminDashboardController.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Van on 02.04.2016.
+ */
+angular.module('Jobsite').controller("AdminDashboardController", function ($rootScope, $scope, ValiDatedTokenObject, locationHistoryService, $location, $modal, $http, $timeout, AuthService, JobsService, ReferralService, RESOURCES, cfpLoadingBar) {
+        $scope.role = AuthService.authentication.isAdministrator ? "Admin" : "User";
+        $scope.entryLimits = [5, 10, 15, 20, 25];
+        $scope.isLoading = true;
+        var serviceBase = RESOURCES.API_BASE_PATH;
+        var _pageCalc = function () {
+            $scope.currentPage = 1; //current page
+            $scope.entryLimit = 10; //max no of items to display in a page
+            $scope.filteredItems = $scope.list.length; //Initially for no filter
+            $scope.totalItems = $scope.list.length;
+        };
+
+        $scope.headingmessage = "Jobs Posted";
+        $scope.viewtext = "View Applicants";
+        $scope.jobsNotFoundMessage = "No jobs";
+        JobsService.getMyJobs().then(function (results) {
+            $scope.list = results.data;
+            _pageCalc();
+            $scope.isLoading = false;
+        }, function (error) {
+            console.log(error.data.message);
+            $scope.isLoading = false;
+        });
+
+    $scope.deleterecords = function (id) {
+        console.log(id);
+        $http({ method: 'DELETE', url: serviceBase + 'jobs/' + id, headers: { 'Content-Type': 'application/json', 'Connection': 'keep-alive', 'Authorization': ValiDatedTokenObject.getValiDatedTokenObject().token_type + " " + ValiDatedTokenObject.getValiDatedTokenObject().access_token } }).
+        success(function (response) {
+            JobsService.getMyJobs().then(function (results) {
+                $scope.list = results.data;
+                _pageCalc();
+            }, function (error) {
+                console.log(error.data.message);
+            });
+        });
+    };
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+});
---
+++ Development2/scripts/controllers/UserDashboardController.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Van on 02.04.2016.
+ */
+angular.module('Jobsite').controller("UserDashboardController", function ($rootScope, $scope, ValiDatedTokenObject, locationHistoryService, $location, $modal, $http, $timeout, AuthService, JobsService, ReferralService, RESOURCES, cfpLoadingBar) {
+   $scope.role = AuthService.authentication.isAdministrator ? "Admin" : "User";
+   $scope.entryLimits = [5, 10, 15, 20, 25];
+   $scope.isLoading = true;
+   var serviceBase = RESOURCES.API_BASE_PATH;
+   var _pageCalc = function () {
+        $scope.currentPage = 1; //current page
+        $scope.entryLimit = 10; //max no of items to display in a page
+        $scope.filteredItems = $scope.list.length; //Initially for no filter
+        $scope.totalItems = $scope.list.length;
+    };
+
+   JobsService.getJobsApplied().then(function (results) {
+            $scope.list = results.data;
+
+            ReferralService.getReferrals().then(function (results2) {
+                $scope.list = $scope.list.concat(results2.data);
+                ReferralService.getReferences().then(function (results3) {
+                    $scope.list = $scope.list.concat(results3.data);
+                    _pageCalc();
+                    $scope.isLoading = false;
+                }, function (error) {
+                    console.log(error.data.message);
+                    $scope.isLoading = false;
+                });
+            }, function (error) {
+                console.log(error.data.message);
+                $scope.isLoading = false;
+            });
+
+        }, function (error) {
+      console.log(error.data.message);
+      $scope.isLoading = false;
+   });
+
+   $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+   $scope.sendReferenceToFriends = function (job, referenceId) {
+        var resumeId = job.resumeId;
+        var jobId = job.id;
+
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: 'views/SendReferenceToFriendsView.html',
+            controller: 'SendReferenceToFriendsController',
+            size : 'lg',
+            resolve: {
+                resumeId: function () {
+                    return resumeId;
+                },
+                jobId: function () {
+                    return jobId;
+                },
+                jobTitle: function () {
+                    return job.title;
+                },
+                referenceId: function () {
+                    return referenceId;
+                }
+            }
+        });
+
+    };
+});

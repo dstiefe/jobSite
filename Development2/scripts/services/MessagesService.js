efada14b4922d031d4ec34d28404ff79a3b539db
Van Korn <vanderkorn@gmail.com>
2016-04-18 02:17:20 +0500

    service for messagestemplates

--- Development2/scripts/services/MessagesService.js
+++ Development2/scripts/services/MessagesService.js
@@ -31,8 +31,49 @@ angular.module('Jobsite').factory('MessagesService', ['$http', '$q', 'RESOURCES'
         });
     };
 
+    var _sendBulkMessage =  function (request) {
+        return $http.post(serviceBase + 'messagesbulk',
+            request,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': ValiDatedTokenObject.getValiDatedTokenObject().token_type+" "+ValiDatedTokenObject.getValiDatedTokenObject().access_token
+                }
+            }).then(function (results) {
+            return results;
+        });
+    };
+
+    var _postMessageTemplate =  function (request) {
+        return $http.post(serviceBase + 'messagetemplates',
+            request,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': ValiDatedTokenObject.getValiDatedTokenObject().token_type+" "+ValiDatedTokenObject.getValiDatedTokenObject().access_token
+                }
+            }).then(function (results) {
+            return results;
+        });
+    };
+
+    var _getMessageTemplates = function(){
+        return $http.get(serviceBase + 'messagetemplates',{
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': ValiDatedTokenObject.getValiDatedTokenObject().token_type+" "+ValiDatedTokenObject.getValiDatedTokenObject().access_token
+            }
+        }).then(function (results) {
+            return results;
+        });
+    };
+
     messagesServiceFactory.sendMessage = _sendMessage;
     messagesServiceFactory.getMessages = _getMessages;
+    messagesServiceFactory.sendBulkMessage = _sendBulkMessage;
+
+    messagesServiceFactory.postMessageTemplate = _postMessageTemplate;
+    messagesServiceFactory.getMessageTemplates = _getMessageTemplates;
 
     return messagesServiceFactory;
 }]);

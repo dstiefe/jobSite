7ad9b5d131350701dc3a7c059516f7029c6fc4f4
Van Korn <vanderkorn@gmail.com>
2016-05-02 17:15:04 +0500

    Hot updates on testing/debug all functionality:
    - In some cases "Go Back" button redirect to incorrect page
    + Issues with sorting /#/resumes - FIXED!
    + UI issue on creation resume (#/resumes) - FIXED!
    + Issues with updating job - FIXED!
    + Refernces/Interviews: sorting by create date is not working - FIXED!

--- Development2/scripts/controllers/ResumesController.js
+++ Development2/scripts/controllers/ResumesController.js
@@ -2,7 +2,7 @@
  * Created by Van on 17.01.2016.
  */
 //Controller for working resumes
-angular.module('Jobsite').controller("ResumesController", function($scope, AuthService, $location, SearchResumesParameters, $modal, ResumesService) {
+angular.module('Jobsite').controller("ResumesController", function($scope, AuthService, $location, SearchResumesParameters, $modal, $timeout, ResumesService) {
 
     $scope.currentPage = 1;
 
